9fe1b3b89a519dd02c9f0ac8f3461db39465d9c6
nipun aggarwal <nipun.aggarwal@glider.ai>
2021-08-19 23:40:32 +0530

    api config added to process env

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -49,7 +49,7 @@ module.exports = {
     externals: {
         // global app config object
         config: JSON.stringify({
-            apiUrl: 'http://localhost:4000'
+            apiUrl: process.env.API_URL || 'http://localhost:4000'
         })
     }
 }
